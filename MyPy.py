43f53ecc003daf108b5ecd583440c39093043f9d
AlexAcmeCorp <alex.acmecorp@outlook.com>
2024-05-19 09:10:30 -0700

    Update MyPy.py

--- MyPy.py
+++ MyPy.py
@@ -1,20 +1,16 @@
-
 expenses = []
 
-def add_expense(details, amount):
-    expense = {'details': details, 'amount': amount}
+def add_expense(expense_detail, expense_amount):
+    expense = {'detail': expense_detail, 'amount': expense_amount}
     expenses.append(expense)
 
-def delete_expense(identifier):
-    for expense in expenses:
-        if expense['details'] == identifier:
-            expenses.remove(expense)
+def delete_expense(expense_id):
+    if expense_id < len(expenses):
+        del expenses[expense_id]
 
-def update_expense(identifier, new_details, new_amount):
-    for expense in expenses:
-        if expense['details'] == identifier:
-            expense['details'] = new_details
-            expense['amount'] = new_amount
+def update_expense(expense_id, new_detail, new_amount):
+    if expense_id < len(expenses):
+        expenses[expense_id] = {'detail': new_detail, 'amount': new_amount}
 
 def view_expenses():
     for expense in expenses:
